76df8339785b19c057362afcb75558e1935e6eaa
compgirl123 <claudia.f.feochari@hotmail.com>
2023-10-20 18:25:17 -0400

    Added .sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,16 @@
 cd ./package.json
-# First npm install command\
+# First npm install command
 echo "Running First npm install..."
 npm install
 
-# Second npm install command\
+# Second npm install command
 echo "Running Second npm install..."
 cd client
 npm install
 
-# Third npm install command\
+# Third npm install command
 echo "Running Third npm install..."
 cd server
 npm install
 
-# Additional setup steps\
-# ...\
 

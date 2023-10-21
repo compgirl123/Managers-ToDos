a41ebff2f6eff351c8748247215c045fbb2b6d67
compgirl123 <claudia.f.feochari@hotmail.com>
2023-10-21 10:42:20 -0400

    Added cors fix

--- server/server.js
+++ server/server.js
@@ -22,11 +22,14 @@ const db = mysql.createPool({
   connectionLimit: 10
 })
 // Set up the CORS headers
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://managers-todos0.onrender.com");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  console.log('CORS headers applied');
+app.use((req, res, next) => {
+  //res.header("Access-Control-Allow-Origin", "https://managers-todos0.onrender.com");
+  //res.setHeader("Access-Control-Allow-Origin", "https://managers-todos0.onrender.com");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
